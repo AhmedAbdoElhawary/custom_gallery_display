02c674337ea41d595520f898b851ae31051ea03b
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-10-20 01:18:45 +0200

    init files

---
+++ lib/src/entities/tabs_texts.dart
@@ -0,0 +1,25 @@
+class TabsTexts {
+  final String videoText;
+  final String photoText;
+  final String galleryText;
+  final String deletingText;
+  final String limitingText;
+  final String holdButtonText;
+  final String clearImagesText;
+  final String notFoundingCameraText;
+  final String noImagesFounded;
+  final String acceptAllPermissions;
+
+  TabsTexts({
+    this.videoText = "VIDEO",
+    this.photoText = "PHOTO",
+    this.clearImagesText = "Clear selected images",
+    this.galleryText = "GALLERY",
+    this.deletingText = "DELETE",
+    this.limitingText = "The limit is 10 photos or videos.",
+    this.notFoundingCameraText = "No secondary camera found",
+    this.holdButtonText = "Press and hold to record",
+    this.noImagesFounded = "There is no images",
+    this.acceptAllPermissions = "Failed! accept all access permissions.",
+  });
+}
